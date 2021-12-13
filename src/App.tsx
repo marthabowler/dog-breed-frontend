d82e7555ff0620678f6a5ecad891c6ea3fe79b91
Jenna <jr13114@my.bristol.ac.uk>
2021-12-13 16:27:57 +0000

    formatted and removed greet test.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import FetchPhotos from "./components/FetchPhotos";
-import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return <>
- <FetchPhotos />
-  </>
+  return (
+    <>
+      <FetchPhotos />
+    </>
+  );
 }
 
 export default App;
--- src/utils/getBreed.ts
+++ src/utils/getBreed.ts
@@ -1,5 +1,5 @@
 export function getBreed(url: string): string {
-    const arr = url.split("/");
-    // console.log(arr[4]);
-    return arr[4]
+  const arr = url.split("/");
+  // console.log(arr[4]);
+  return arr[4];
 }
