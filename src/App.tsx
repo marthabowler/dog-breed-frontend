a92b4448eb496edb63281c9868089535a7153962
Jenna <jr13114@my.bristol.ac.uk>
2021-12-14 15:36:40 +0000

    created Leaderboard.tsx fetched leaderboard data and showed in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import FetchPhotos from "./components/FetchPhotos";
+import Leaderboard from "./components/Leaderboard";
 
 function App(): JSX.Element {
   return (
     <>
       <FetchPhotos />
+      <Leaderboard />
     </>
   );
 }
